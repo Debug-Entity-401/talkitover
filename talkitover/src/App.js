4e9183dc4cd4a25fc97f7b9333ca3aecafe631d9
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-07-26 09:32:53 +0300

    register edits

--- talkitover/src/App.js
+++ talkitover/src/App.js
@@ -3,6 +3,8 @@ import Register from './components/Register/Register';
 import LoginAccess from './components/login-access';
 import UserExist from './components/user-exist';
 import Oauth from './components/Oauth/Oauth';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Alert } from 'react-bootstrap'
 function App() {
   return (
     <div className="App">
@@ -12,10 +14,14 @@ function App() {
 <Register/>
 {/* <Oauth/> */}
 <LoginAccess>
-  <h2>hi every body u are logged in</h2>
+<Alert variant="success" className="user-msg">
+  hi every body u are logged in
+  </Alert>
 </LoginAccess>
 <UserExist>
-  <h1>the user already exist</h1>
+  <Alert variant="warning" className="user-msg">
+  the user already exist
+  </Alert>
 </UserExist>
 
     </div>
