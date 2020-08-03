1f551dd96b8c852f6c1a6bf3516190d9b0303e7d
sondoshassan <sondos199628@gmail.com>
2020-08-03 22:45:04 +0300

    finish profile page

--- talkitover/src/components/Profile/Profile.js
+++ talkitover/src/components/Profile/Profile.js
@@ -6,17 +6,47 @@ import {
   fetchData,
   updateProfile,
 } from "../../store/profile-store";
+import Divider from '@material-ui/core/Divider';
+import clsx from 'clsx';
 import { Image, Container, Modal, Button, Form, Navbar } from "react-bootstrap";
 import { CountryDropdown } from "react-country-region-selector";
 import Reviews from "./reviews";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./profile.scss";
+import { IconButton, InputAdornment, FormControl, Input, TextField, MenuItem } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import {Row,Col} from 'react-bootstrap';
 import Sidebar from '../Sidebar/Sidebar';
 
 
+
 var country1 = "Select Country";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: '95%',
+  },
+  input: {
+    display: 'none',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    width:'98%'
+  },
+}));
 const Profile = (props) => {
+  const classes = useStyles();
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -27,9 +57,40 @@ const Profile = (props) => {
 
   const onChangeHandler = async (event) => {
     if (event.target) {
-      props.adding({
-        [event.target.name]: event.target.value,
-      });
+      if (event.target.files && event.target.files[0]) {
+        //////////////////////////////
+        let image = document.getElementById('uploadImage');
+        // Check if any file is selected. 
+        if (image.files.length > 0) {
+          for (let i = 0; i <= image.files.length - 1; i++) {
+
+            const fsize = image.files.item(i).size;
+            const file = Math.round((fsize / 1024));
+            // The size of the file. 
+            if (file >= 100) {
+              alert(
+                "File must be lest 100 kb");
+            }
+          }
+        }
+        let reader = new FileReader();
+        reader.onload = (e) => {
+          // this.setState({image: e.target.result});
+          props.adding({ photo: e.target.result });
+        };
+        reader.readAsDataURL(event.target.files[0]);
+
+        // ////////////////////////////////
+        // console.log('///////////////',typeof URL.createObjectURL(event.target.files[0]));
+        // props.add({[event.target.name]: URL.createObjectURL(event.target.files[0])});
+
+      } else {
+
+        props.adding({
+          [event.target.name]: event.target.value,
+        });
+      }
+      
     } else {
       country1 = event;
       props.adding({
@@ -64,21 +125,21 @@ const Profile = (props) => {
       <Sidebar />
 </Col>
 <Col xs={6} sm={6} md={11}>
+<div className="waves">
+<h2 className='user-name'>{props.profile.results.username}</h2>
+<div className="img-container">{photoRender()}</div>
+    <svg  id="parent-wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#6fc5ba" fill-opacity="1" d="M0,288L40,293.3C80,299,160,309,240,288C320,267,400,213,480,186.7C560,160,640,160,720,176C800,192,880,224,960,245.3C1040,267,1120,277,1200,261.3C1280,245,1360,203,1400,181.3L1440,160L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
+</svg>
+</div>
       <Container className="profile-container">
-        <Navbar className="profile-nav">
+        {/* <Navbar className="profile-nav">
           <Navbar.Brand>
-            <h2>{props.profile.results.username}</h2>
+           
           </Navbar.Brand>
-        </Navbar>
+        </Navbar> */}
         <section className="profile">
-          <div className="edit-container">
-            <div className="img-container">{photoRender()}</div>
-            <div className="edit-btn">
-              <Button variant="primary" onClick={handleShow}>
-                Edit Profile
-              </Button>
-            </div>
-          </div>
+         
           <div className="profile-data">
             <ul>
               <li>
@@ -86,22 +147,35 @@ const Profile = (props) => {
                   <b>Email</b> : {props.profile.results.email}
                 </h5>
               </li>
+              <Divider variant="middle" component="li" />
               <li>
                 <h5>
                   <b>Country</b> : {props.profile.results.country}
                 </h5>
               </li>
+              <Divider variant="middle" component="li" />
+
               <li>
                 <h5>
                   <b>Role</b> : {props.profile.results.role}
                 </h5>
               </li>
+              <Divider variant="middle" component="li" />
+
               <li>
                 <h5>
                   <b>Status</b> : {props.profile.results.status}
                 </h5>
               </li>
             </ul>
+          </div>
+          <div className="edit-container">
+            
+            <div className="edit-btn">
+              <Button variant="primary" onClick={handleShow}>
+                Edit Profile
+              </Button>
+            </div>
           </div>
           <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
@@ -110,22 +184,56 @@ const Profile = (props) => {
             <Modal.Body>
               <Form onSubmit={onUpdate}>
                 <Form.Group controlId="formBasicEmail">
-                  <Form.Label>user Name</Form.Label>
-                  <Form.Control
+                  {/* <Form.Label>user Name</Form.Label> */}
+                  <TextField className={clsx(classes.margin, classes.textField)}
+                    id="standard-textarea"
+                    label="Username"
+                    defaultValue={props.profile.results.username}
+                    placeholder="username" name='user_name' onChange={onChangeHandler}
+                    multiline
+                  />
+                  <br/>
+                  {/* <Form.Control
                     type="userName"
                     name="username"
                     placeholder="Enter username"
                     defaultValue={props.profile.results.username}
                     onChange={onChangeHandler}
-                  ></Form.Control>
-                  <Form.Label>Email address</Form.Label>
+                  ></Form.Control> */}
+                  {/* <Form.Label>Email address</Form.Label>
                   <Form.Control
                     type="email"
+                    //////for image
                     name="email"
                     placeholder="Enter email"
                     defaultValue={props.profile.results.email}
                     onChange={onChangeHandler}
-                  ></Form.Control>
+                  ></Form.Control> */}
+                  <TextField className={clsx(classes.margin, classes.textField)}
+                    id="standard-textarea"
+                    defaultValue={props.profile.results.email}
+                    label="Email"
+                    placeholder="email" name='email' onChange={onChangeHandler}
+                    multiline
+                  />
+                  <br/>
+                 
+                  <input
+                    accept="image/*"
+                    className={`${classes.input} uploadImage`} name="photo"
+                    onChange={onChangeHandler}
+                    // defaultValue={props.profile.results.photo}
+                    required={true}
+                    multiple
+                    type="file"
+                  />
+                  <label htmlFor="uploadImage" className="profile-label">
+                  Profile Picture
+                  <br/>
+                    <Button variant="contained" style={{ color: '#fff' }} color="primary" component="span">
+                      Upload 
+        </Button>
+                  </label>
                 </Form.Group>
                 <Form.Group controlId="formBasicPassword">
                   <Form.Label>Country</Form.Label>
@@ -135,7 +243,7 @@ const Profile = (props) => {
                     onChange={onChangeHandler}
                   />
                 </Form.Group>
-                <Form.Group>
+                {/* <Form.Group>
                   <Form.Label>Photo</Form.Label>
                   <Form.Control
                     type="text"
@@ -144,7 +252,7 @@ const Profile = (props) => {
                     defaultValue={props.profile.results.photo}
                     onChange={onChangeHandler}
                   />
-                </Form.Group>
+                </Form.Group> */}
                 <Modal.Footer>
                   <Button variant="primary" type="submit">
                     Save Changes
