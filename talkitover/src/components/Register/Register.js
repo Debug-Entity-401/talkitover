c76765e3748837b89b565cd6ef3c78efdc32542d
Bushra-B <bushraxbil@gmail.com>
2020-08-05 01:19:11 +0300

    signup form styling

--- talkitover/src/components/Register/Register.js
+++ talkitover/src/components/Register/Register.js
@@ -46,6 +46,7 @@ const useStyles = makeStyles((theme) => ({
 
 
 var country1 = "Select Country";
+
 function Register(props) {
   const classes = useStyles();
   const [redirict, setRedircit] = useState('');
@@ -74,13 +75,11 @@ function Register(props) {
         setValues({ ...values, password: event.target.value });
       }
 
-
-
-
       if (event.target.files && event.target.files[0]) {
         //////////////////////////////
         let image = document.getElementById('uploadImage');
-        // Check if any file is selected. 
+        // Check if any file is selected.
+        let count = 0; 
         if (image.files.length > 0) {
           for (let i = 0; i <= image.files.length - 1; i++) {
 
@@ -88,17 +87,22 @@ function Register(props) {
             const file = Math.round((fsize / 1024));
             // The size of the file. 
             if (file >= 100) {
-              alert(
-                "File must be lest 100 kb");
-            }
+            //   alert(
+            //     "File must be lest 100 kb");
+            // }
+            count++;
+            document.getElementById('upload-img').innerText = 'File must be less than 100 kb'
           }
         }
-        let reader = new FileReader();
-        reader.onload = (e) => {
-          // this.setState({image: e.target.result});
-          props.add({ photo: e.target.result });
-        };
-        reader.readAsDataURL(event.target.files[0]);
+        if(count === 0) {
+          let reader = new FileReader();
+          reader.onload = (e) => {
+            // this.setState({image: e.target.result});
+            document.getElementById('upload-img').innerText = 'Image Uploaded';
+            props.add({ photo: e.target.result });
+          };
+          reader.readAsDataURL(event.target.files[0]);
+        }
 
         // ////////////////////////////////
         // console.log('///////////////',typeof URL.createObjectURL(event.target.files[0]));
@@ -116,6 +120,7 @@ function Register(props) {
       });
     }
   }
+}
  
   const handelSubmit = async (e) => {
     e.preventDefault();
@@ -130,7 +135,7 @@ function Register(props) {
   }
   return (
     <>
-      <Container>
+      <Container id="reg-cont">
         <div className="signup">
           <Form className="signup-form" onSubmit={handelSubmit}>
             <Row>
@@ -187,9 +192,12 @@ function Register(props) {
                   <label htmlFor="uploadImage" id="profile-label">
                   Profile Picture
                   <br/>
+                  <div className="image-upload">
                     <Button variant="contained" style={{ color: '#fff' }} color="primary" component="span">
                       Upload 
-        </Button>
+                  </Button>
+                  <span id="upload-img"></span>
+                  </div>
                   </label>
                   <br />
                   <FormControl className={classes.formControl}>
@@ -250,6 +258,7 @@ function Register(props) {
     </>
   );
 }
+
 
 const mapStateToProps = (state) => ({
   signUp: state.signUp,
