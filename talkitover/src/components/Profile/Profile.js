92313ef904c08f6ee683c121d6c592c57b75b6ad
sondoshassan <sondos199628@gmail.com>
2020-08-04 15:16:50 +0300

    add default for edit

--- talkitover/src/components/Profile/Profile.js
+++ talkitover/src/components/Profile/Profile.js
@@ -8,14 +8,14 @@ import {
 } from "../../store/profile-store";
 import Divider from '@material-ui/core/Divider';
 import clsx from 'clsx';
-import { Image, Container, Modal, Button, Form} from "react-bootstrap";
+import { Image, Container, Modal, Button, Form } from "react-bootstrap";
 import { CountryDropdown } from "react-country-region-selector";
 import Reviews from "./reviews";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./profile.scss";
 import { TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import {Row,Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import Sidebar from '../Sidebar/Sidebar';
 
 
@@ -42,7 +42,7 @@ const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
-    width:'98%'
+    width: '98%'
   },
 }));
 const Profile = (props) => {
@@ -90,7 +90,7 @@ const Profile = (props) => {
           [event.target.name]: event.target.value,
         });
       }
-      
+
     } else {
       country1 = event;
       props.adding({
@@ -100,11 +100,14 @@ const Profile = (props) => {
   };
   const onUpdate = async (event) => {
     event.preventDefault();
-    if(!props.profile.profile.user_name) props.adding({ user_name: props.profile.results.user_name });
-    if(!props.profile.profile.email) props.adding({ email: props.profile.results.email });
-    if(!props.profile.profile.photo) props.adding({ photo: props.profile.results.photo });
+    let obj = {};
+    if (!props.profile.profile.user_name) obj.user_name = props.profile.results.user_name; else obj.user_name = props.profile.profile.user_name;
+    if (!props.profile.profile.email) obj.email = props.profile.results.email; else obj.email = props.profile.profile.email;
+    if (!props.profile.profile.photo) obj.photo = props.profile.results.photo; else obj.photo = props.profile.profile.photo;
+    if (!props.profile.profile.country) obj.country = props.profile.results.country; else obj.country = props.profile.profile.country;
     // let obj= {};
-    props.updateProfile(props.profile.results.id, props.profile.profile);
+    console.log('obj in profile ===> ',obj);
+    props.updateProfile(props.profile.results.id, obj);
     props.fetchData();
     setShow(false);
   };
@@ -124,92 +127,92 @@ const Profile = (props) => {
   };
   return (
     <>
-    <Row>
-    <Col xs={6} sm={6} md={1}>
-      <Sidebar />
-</Col>
-<Col xs={6} sm={6} md={11}>
-<div className="waves">
-<h2 className='user-name'>{props.profile.results.username}</h2>
-<div className="img-container">{photoRender()}</div>
-    <svg  id="parent-wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-  <path fill="#6fc5ba" fill-opacity="1" d="M0,288L40,293.3C80,299,160,309,240,288C320,267,400,213,480,186.7C560,160,640,160,720,176C800,192,880,224,960,245.3C1040,267,1120,277,1200,261.3C1280,245,1360,203,1400,181.3L1440,160L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
-</svg>
-</div>
-      <Container className="profile-container">
-        {/* <Navbar className="profile-nav">
+      <Row>
+        <Col xs={6} sm={6} md={1}>
+          <Sidebar />
+        </Col>
+        <Col xs={6} sm={6} md={11}>
+          <div className="waves">
+            <h2 className='user-name'>{props.profile.results.username}</h2>
+            <div className="img-container">{photoRender()}</div>
+            <svg id="parent-wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+              <path fill="#6fc5ba" fill-opacity="1" d="M0,288L40,293.3C80,299,160,309,240,288C320,267,400,213,480,186.7C560,160,640,160,720,176C800,192,880,224,960,245.3C1040,267,1120,277,1200,261.3C1280,245,1360,203,1400,181.3L1440,160L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
+            </svg>
+          </div>
+          <Container className="profile-container">
+            {/* <Navbar className="profile-nav">
           <Navbar.Brand>
            
           </Navbar.Brand>
         </Navbar> */}
-        <section className="profile">
-         
-          <div className="profile-data">
-            <ul>
-              <li>
-                <h5>
-                  <b>Email</b> : {props.profile.results.email}
-                </h5>
-              </li>
+            <section className="profile">
 
-              <Divider variant="middle" component="li" />
-              <br/>
-              <li>
-                <h5>
-                  <b>Country</b> : {props.profile.results.country}
-                </h5>
-              </li>
-              <Divider variant="middle" component="li" />
-              <br/>
+              <div className="profile-data">
+                <ul>
+                  <li>
+                    <h5>
+                      <b>Email</b> : {props.profile.results.email}
+                    </h5>
+                  </li>
 
-              <li>
-                <h5>
-                  <b>Role</b> : {props.profile.results.role}
-                </h5>
-              </li>
-              <Divider variant="middle" component="li" />
-              <br/>
+                  <Divider variant="middle" component="li" />
+                  <br />
+                  <li>
+                    <h5>
+                      <b>Country</b> : {props.profile.results.country}
+                    </h5>
+                  </li>
+                  <Divider variant="middle" component="li" />
+                  <br />
 
-              <li>
-                <h5>
-                  <b>Status</b> : {props.profile.results.status}
-                </h5>
-              </li>
+                  <li>
+                    <h5>
+                      <b>Role</b> : {props.profile.results.role}
+                    </h5>
+                  </li>
+                  <Divider variant="middle" component="li" />
+                  <br />
 
-            </ul>
-          </div>
-          <div className="edit-container">
-            
-            <div className="edit-btn">
-              <Button variant="primary" onClick={handleShow}>
-                Edit Profile
+                  <li>
+                    <h5>
+                      <b>Status</b> : {props.profile.results.status}
+                    </h5>
+                  </li>
+
+                </ul>
+              </div>
+              <div className="edit-container">
+
+                <div className="edit-btn">
+                  <Button variant="primary" onClick={handleShow}>
+                    Edit Profile
               </Button>
-            </div>
-          </div>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Edit Your Profile</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form onSubmit={onUpdate}>
-                <Form.Group controlId="formBasicEmail">
-                  {/* <Form.Label>user Name</Form.Label> */}
-                  <TextField className={clsx(classes.margin, classes.textField)}
-                    id="standard-textarea"
-                    label="Username"
-                    value={props.profile.results.username}
-                    placeholder="username" name='user_name' onChange={onChangeHandler}
-                    multiline
-                  />
-                  <br/>
-                  {/* <Form.Control
+                </div>
+              </div>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Edit Your Profile</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form onSubmit={onUpdate}>
+                    <Form.Group controlId="formBasicEmail">
+                      {/* <Form.Label>user Name</Form.Label> */}
+                      <TextField className={clsx(classes.margin, classes.textField)}
+                        id="standard-textarea"
+                        label="Username"
+                        value={props.profile.results.username}
+                        placeholder="username" name='user_name' onChange={onChangeHandler}
+                        multiline
+                      />
+                      <br />
+                      {/* <Form.Control
                     type="userName"
                     name="username"
                     placeholder="Enter username"
                     defaultValue={props.profile.results.username}
                     onChange={onChangeHandler}
                   ></Form.Control> */}
-                  {/* <Form.Label>Email address</Form.Label>
+                      {/* <Form.Label>Email address</Form.Label>
                   <Form.Control
                     type="email"
                     //////for image
@@ -218,41 +221,41 @@ const Profile = (props) => {
                     defaultValue={props.profile.results.email}
                     onChange={onChangeHandler}
                   ></Form.Control> */}
-                  <TextField className={clsx(classes.margin, classes.textField)}
-                    id="standard-textarea"
-                    defaultValue={props.profile.results.email}
-                    label="Email"
-                    placeholder="email" name='email' onChange={onChangeHandler}
-                    multiline
-                  />
-                  <br/>
-                 
-                  <input
-                    accept="image/*"
-                    className={`${classes.input} uploadImage`} name="photo"
-                    onChange={onChangeHandler}
-                    // defaultValue={props.profile.results.photo}
-                    
-                    multiple
-                    type="file"
-                  />
-                  <label htmlFor="uploadImage" className="profile-label">
-                  Profile Picture
-                  <br/>
-                    <Button variant="contained" style={{ color: '#fff' }} color="primary" component="span">
-                      Upload 
+                      <TextField className={clsx(classes.margin, classes.textField)}
+                        id="standard-textarea"
+                        defaultValue={props.profile.results.email}
+                        label="Email"
+                        placeholder="email" name='email' onChange={onChangeHandler}
+                        multiline
+                      />
+                      <br />
+
+                      <input
+                        accept="image/*"
+                        className={`${classes.input} uploadImage`} name="photo"
+                        onChange={onChangeHandler}
+                        // defaultValue={props.profile.results.photo}
+
+                        multiple
+                        type="file"
+                      />
+                      <label htmlFor="uploadImage" className="profile-label">
+                        Profile Picture
+                  <br />
+                        <Button variant="contained" style={{ color: '#fff' }} color="primary" component="span">
+                          Upload
         </Button>
-                  </label>
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Country</Form.Label>
-                  <CountryDropdown
-                    name="rcrs-country"
-                    defaultOptionLabel={country1}
-                    onChange={onChangeHandler}
-                  />
-                </Form.Group>
-                {/* <Form.Group>
+                      </label>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Country</Form.Label>
+                      <CountryDropdown
+                        name="rcrs-country"
+                        defaultOptionLabel={country1}
+                        onChange={onChangeHandler}
+                      />
+                    </Form.Group>
+                    {/* <Form.Group>
                   <Form.Label>Photo</Form.Label>
                   <Form.Control
                     type="text"
@@ -262,21 +265,21 @@ const Profile = (props) => {
                     onChange={onChangeHandler}
                   />
                 </Form.Group> */}
-                <Modal.Footer>
-                  <Button variant="primary" type="submit">
-                    Save Changes
+                    <Modal.Footer>
+                      <Button variant="primary" type="submit">
+                        Save Changes
                   </Button>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
+                      <Button variant="secondary" onClick={handleClose}>
+                        Close
                   </Button>
-                </Modal.Footer>
-              </Form>
-            </Modal.Body>
-          </Modal>
-        </section>
-        <Reviews />
-      </Container>
-      </Col>
+                    </Modal.Footer>
+                  </Form>
+                </Modal.Body>
+              </Modal>
+            </section>
+            <Reviews />
+          </Container>
+        </Col>
       </Row>
     </>
   );
